6e6e0b221221ed53400101828afaa2493a0f46cf
artur <khalikart@gmail.com>
2021-12-08 19:38:22 +0500

    redirect after POST and show all elements in the tamplate

--- fun_tests.py
+++ fun_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
         
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -26,7 +31,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('make a spot')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        time.sleep(3)
         
         
         table = self.browser.find_element_by_id('id_list_table')
