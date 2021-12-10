3fa7afb736302d6983e713983014134ae50bf6a9
artur <khalikart@gmail.com>
2021-12-10 16:38:46 +0500

    added URL for a new list throught POST

--- lists/urls.py
+++ lists/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . views import home_page
+from . views import home_page, view_list, new_list
 
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', home_page, name='home'),
+    path('lists/new', new_list, name='new_list'),
+    path('lists/the_one_of', view_list, name='view_list'),
 ]
